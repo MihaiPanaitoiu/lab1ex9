ef7f788755d42e15958bf30bfbc7ff5d446a56ee
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2022-11-27 20:02:36 +0200

    Add project files.

---
+++ lab1ex9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab1ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ex 9
+            Scrieti un program care interschimba valoarea a doua variabile intregi.
+             */
+            Console.WriteLine("a = ");
+            int a = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("b = ");
+            int b = int.Parse(Console.ReadLine());
+
+            (b, a) = (a, b);
+
+            Console.WriteLine($"a = {a}, b = {b}");
+        }
+    }
+}
